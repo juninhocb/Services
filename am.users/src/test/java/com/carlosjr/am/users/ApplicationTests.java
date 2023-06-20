4a5003b17a02e9b5c063a44cf83f2a3047034653
juninhocb <juninhocb@homail.com>
2023-06-20 18:36:51 -0300

    init project and database/profiles config - closes #1

---
+++ am.users/src/test/java/com/carlosjr/am/users/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosjr.am.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
